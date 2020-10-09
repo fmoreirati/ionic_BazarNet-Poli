a3d2c7db18fef4263ac283d818a5ebde2fe130b4
fMoreiraTI <fabianomoreira.ti@gmail.com>
2020-10-09 20:50:48 -0300

    Aula 06

--- src/app/pages/usuario-list/usuario-list.page.ts
+++ src/app/pages/usuario-list/usuario-list.page.ts
@@ -1,6 +1,10 @@
 import { Component, OnInit } from '@angular/core';
-import { Usuario } from 'src/app/models/usuario';
+import { Router } from '@angular/router';
+import { AlertController } from '@ionic/angular';
 import { UsuarioService } from 'src/app/services/usuario.service';
+import { Usuario } from 'src/app/models/usuario';
+import { MessageService } from '../../services/message.service';
+
 
 @Component({
   selector: 'app-usuario-list',
@@ -9,10 +13,14 @@ import { UsuarioService } from 'src/app/services/usuario.service';
 })
 export class UsuarioListPage implements OnInit {
 
-public usuarios:Usuario[] = []
+  public usuarios: Usuario[] = []
 
   constructor(
-    private usuarioService: UsuarioService
+    private usuarioService: UsuarioService,
+    private router: Router,
+    private alertController: AlertController,
+    private msg: MessageService
+   
   ) { }
 
   ngOnInit() {
@@ -22,5 +30,52 @@ public usuarios:Usuario[] = []
       }
     )
   }
+
+  // remover(id) {
+  //   this.usuarioService.remover(id).then(
+  //     () => {
+  //       this.router.navigate([""]);
+  //     },
+  //     err => {
+  //       console.log("Erro: ", err);
+  //     }
+  //   )
+  // }
+
+  async remover(id) {
+    const alert = await this.alertController.create({
+      cssClass: 'my-custom-class',
+      header: 'Confirmar!',
+      message: 'Deseja apagar os dados do usuário?',
+      buttons: [
+        {
+          text: 'Cancelar',
+          role: 'cancel',
+          cssClass: 'secondary',
+          handler: (blah) => {
+            console.log('Confirm Cancel: blah');
+          }
+        }, {
+          text: 'OK',
+          handler: () => {
+            this.msg.presentLoading();
+            this.usuarioService.remover(id).then(
+              () => {
+                this.msg.dismissLoading()
+                this.router.navigate([""]);
+              },
+              err => {
+                this.msg.dismissLoading()
+                console.log("Erro: ", err);
+              }
+            )
+          }
+        }
+      ]
+    });
+    await alert.present();
+  }
+
+ 
 
 }
--- src/app/services/usuario.service.ts
+++ src/app/services/usuario.service.ts
@@ -16,9 +16,8 @@ export class UsuarioService {
   ) { }
 
   add(usuario: Usuario) {
-    return this.fireDB.collection<Usuario>(this.colletionUser).add(
+    return this.fireDB.collection(this.colletionUser).add(
       {
-        id:usuario.id,
         nome: usuario.nome,
         email: usuario.email,
         tel: usuario.tel,
@@ -39,6 +38,16 @@ export class UsuarioService {
 
   get(id:string){
     return this.fireDB.collection(this.colletionUser).doc<Usuario>(id).valueChanges();
+  }
+
+  update(usuario:Usuario, id:string){
+    usuario.lat = usuario.lng++;
+    usuario.lng = usuario.lat++
+    return this.fireDB.collection(this.colletionUser).doc(id).update(usuario);
+  }
+
+  remover(id:string){
+    return this.fireDB.collection(this.colletionUser).doc(id).delete();
   }
 
 }
